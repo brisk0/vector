13a7c4ff58037808dc7497d8eb8d8c5088b4cca1
brisk <bskblake@gmail.com>
2013-07-16 20:02:32 +0930

    Removed unnecessary Quot() function

--- vector.go
+++ vector.go
@@ -26,11 +26,8 @@ func (a Vec3d) Dot(b Vec3d) float64 {return a[0]*b[0] + a[1]*b[1] + a[2]*b[2]}
 func (a Vec2d) Scale(k float64) Vec2d {return Vec2d{k*a[0], k*a[1]}}
 func (a Vec3d) Scale(k float64) Vec3d {return Vec3d{k*a[0], k*a[1], k*a[2]}}
 
-//Quot returns a vector where b_i = k/a_i.
-func (a Vec3d) Quot(k float64) Vec3d {return Vec3d{k/a[0], k/a[1], k/a[2]}}
-
 //Inv returns the multiplicative inverse (reciprocal) of a.
-func (a Vec3d) Inv() Vec3d {return a.Quot(1)}
+func (a Vec3d) Inv() Vec3d {return Vec3d{1/a[0], 1/a[1], 1/a[2]}}
 
 //AbsSquared returns the square of the absolute value of a vector.
 func (a Vec3d) AbsSquared() float64 {return a[0]*a[0] + a[1]*a[1] + a[2]*a[2]}
@@ -40,8 +37,8 @@ func (a Vec3d) Abs() float64 {return math.Sqrt(a.AbsSquared())}
 
 //Dir() returns the direction vector (unit vector) of a.
 func (a Vec3d) Dir() Vec3d {
-	abs := a.Abs()
-	return Vec3d{a[0] / abs, a[1] / abs, a[2] / abs}
+	invAbs := 1/a.Abs()
+	return Vec3d{a[0] * invAbs, a[1] * invAbs, a[2] * invAbs}
 }
 
 /*Rotate() returns a rotated angle radians about unit vector axis using
